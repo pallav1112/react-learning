961be771aafcaae65d78583a55dc2737061d4d14
pallav singh <pallav.singh@onewabash.com>
2026-03-10 08:33:05 -0700

    react first commit

---
+++ react-learning/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Gallery from './gallery'
+import Profile from './profile'
+import ToDoList from './todoList'
+
+function App() {
+ 
+  return (
+    <>
+      <Gallery />
+    </> 
+  )
+}
+
+export default App
